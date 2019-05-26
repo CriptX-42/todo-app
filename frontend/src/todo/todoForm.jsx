c9126311575d0ccb1eea866df63bcbade4cb4ca8
ricardo <ricardo-svc@live.com>
2019-05-26 00:25:52 -0300

    Finalizando a aplicação

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,11 +6,15 @@ import IconButton from '../template/iconButton'
 export default props =>(
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
-                <input id='description' className='form-control' placeholder='Adicione uma terefa'></input>
+                <input id='description' className='form-control' placeholder='Adicione uma terefa'
+                onChange={props.handleChange}
+                value={props.description}></input>
         </Grid>
         <Grid cols='12 3 2'>
             <IconButton style='primary' icon='plus'
             onClick={props.handleAdd}></IconButton>
+            <IconButton style='info' icon='search'
+            onClick={props.handleSearch}></IconButton>
         </Grid>
     </div>
 )
