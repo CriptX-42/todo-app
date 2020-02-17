46f30bd0a7579978ae0c278f183edab5d6e05832
ricardo <ricardo-svc@live.com>
2020-02-17 19:02:01 -0300

    Fazendo requisição com backend

--- frontend/src/template/grid.jsx
+++ frontend/src/template/grid.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 
 export default class Grid extends Component{
     toCssClasses(numbers){
-        const cols = numbers ? numbers.split('') : []
-        let classes = ''
+        const cols = numbers ? numbers.split('') : [];
+        let classes = '';
 
         if(cols[0]) classes += `col-xs-${cols[0]}`
         if(cols[1]) classes += `col-xs-${cols[1]}`
@@ -13,7 +13,7 @@ export default class Grid extends Component{
         return classes
     }
     render(){
-        const gridClasses = this.toCssClasses(this.props.cols || 12)
+        const gridClasses = this.toCssClasses(this.props.cols || 12);
         return (
             <div className={gridClasses}>
                 {this.props.children}
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,23 +1,36 @@
-import React, { Component } from 'react'
-import PageHeader from '../template/pageHeader'
-import TodoForm from './todoForm'
-import TodoList from './todoList'
+import React, { Component } from 'react';
+import axios from 'axios';
 
+
+import PageHeader from '../template/pageHeader';
+import TodoForm from './todoForm';
+import TodoList from './todoList';
+
+
+const URL = 'http://localhost:3003/api/todos';
 
 export default class Todo extends Component{
     constructor(props) {
-        super(props)
-        this.handleAdd = this.handleAdd.bind(this)
+        super(props);
+        this.state = { description: '', list: [] };
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value});
     }
 
     handleAdd() {
-        console.log(this)
+        const description = this.state.description;
+        axios.post(URL, { description })
+            .then(resp => )
     }
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm handleAdd={this.handleAdd}></TodoForm>
+                <TodoForm description={ this.state.description } handleChange={this.handleChange} handleAdd={ this.handleAdd }></TodoForm>
                 <TodoList></TodoList>
             </div>
            
